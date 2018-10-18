60824a26688b91594c6748ba7e2db1f2bd22c38a
Radoslav Georgiev <radorado@hacksoft.io>
2018-10-18 20:27:22 +0300

    Add click handler examples

--- events-and-state/src/App.js
+++ events-and-state/src/App.js
@@ -11,16 +11,25 @@ class App extends React.Component {
       this.setState({});
     }, 5000);
   }
-  handleClick = () => {
+  handleClick = value => {
     console.log('Clicked');
+    console.log(value);
   };
 
   render() {
     console.log('In App render');
 
+    const a = [1, 2];
+    const b = [2, 3];
+
     return (
       <div className="App">
         <Counter />
+        <button onClick={() => this.handleClick(a)}>{a[0]} + {a[1]}</button>
+
+        <button onClick={this.handleClick(b)}>{b[0]} + {b[1]}</button>
+
+        <button onClick={this.handleClick}>asdf</button>
       </div>
     );
   }
